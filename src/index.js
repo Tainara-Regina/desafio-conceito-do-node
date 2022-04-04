8f79423f6b491fd6ab996c705ef6c5e1f7ac371b
Tainara Regina <tainararegina20@gmail.com>
2022-04-04 13:17:35 -0300

    testes concluidos

--- src/index.js
+++ src/index.js
@@ -21,8 +21,6 @@ function checksExistsUserAccount(request, response, next) {
     return next();
 }
 
-
-
 function checkIqualsUserAccount (request, response, next ) {
 const { username } = request.body;
 
@@ -81,31 +79,43 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   const todos = user.todos.find((todos) => todos.id === id );
 
-  todos.title = title;
-  todos.deadline = deadline;
-
-  return response.status(200).json({mensage: "atividade atualizada com sucesso",todos});
+  if(todos){
+        todos.title = title;
+        todos.deadline = new Date(deadline);
+        return response.status(200).json(todos);
+  }else{
+        return response.status(404).json({error: "atividade não encontrada"});
+  }
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   const { user } = request;
   const { id } = request.params;
 
-  const todos = user.id.find((todos) => todos.id === id );
-  todos.done = true;
+  const todo = user.todos.find((todo) => todo.id === id );
+  if(todo){
+    todo.done = true;
+    return response.json(todo)
+  }else{
+    return response.status(404).json({error: "atividade não encontrada."})
+  }
+  
 
-  return response.status(200).json({mensage: "atividade marcada como feito"})
+  
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
   const { user } = request;
   const { id } = request.params;
-  const todos = user.id.find((todos) => todos.id === id);
-  todos.splice(todos,1);
 
-  return response.status(200).json({mensage: "tarefa excluida com sucesso", todos});
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
 
+ if(todoIndex === -1){
+    return response.status(404).json({error: "tarefa não encontrada"});
+ }
+  user.todos.splice(todoIndex, 1);
+  return response.status(204).json();
+ 
 
 });
 
